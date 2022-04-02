a2734cff8f82decfaa6d582d7179458398728f97
eon <noe.navarro1019@gmail.com>
2022-04-02 12:29:55 -0700

    initial project structure

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+const { Schema } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match an email address!']
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,3 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+
