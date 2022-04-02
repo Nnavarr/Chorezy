dd462d2d9ffff7463910609c720686729f1bdba1
eon <noe.navarro1019@gmail.com>
2022-04-02 15:53:21 -0700

    added users query within resolvers and tested connectvity to GraphQL

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,19 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 
-// const resolvers = {
-//   Query: {
-//     me: async (parent, args, context) => {
+const resolvers = {
+  Query: {
+    // TODO: Add authentication
+  //   me: async (parent, args) => {
+  //     const userData = await User.findONe()
+  //   }
+  // }
 
-//     }
-//   }
-// }
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+    }
+  }
+}
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
-const {typeDefs} = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
 // const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
@@ -11,9 +11,9 @@ const app = express();
 
 const startServer = async () => {
   const server = new ApolloServer({
-    typeDefs
-    // resolvers,
-    // context: authMiddleware,
+    typeDefs,
+    resolvers
+    // context: authMiddleware
   });
   await server.start();
   server.applyMiddleware({ app });
@@ -25,14 +25,14 @@ startServer()
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// // Serve up static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
