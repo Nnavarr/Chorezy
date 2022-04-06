d45c0668cb007415415c900c243e81f7ccd6e2ec
eon <noe.navarro1019@gmail.com>
2022-04-05 21:52:55 -0700

    created Child model to be used in conjunction with the User (admin) model.  Additionally, added the resolver/typeDef functionality

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,12 +13,19 @@ const typeDefs = gql`
     username: String
     email: String
     age: Int!
-    type: String
+    children: [Child]
+  }
+
+  type Child {
+    _id: ID!
+    username: String
+    email: String
+    age: Int!
   }
 
   type Query {
-    me: User
     users: [User]
+    children: [Child]
     user(username: String!): User
   }
 
--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -2,15 +2,15 @@ const faker = require('faker');
 
 // import connection and models
 const db = require('../config/connection');
-const { User, Task } = require('../models/index');
+const { User, Task, Child } = require('../models/index');
 
 db.once('open', async() => {
   await Task.deleteMany({});
   await User.deleteMany({});
+  await Child.deleteMany({});
 
   // create user data
   const userData = [];
-  const childData = [];
 
   // create fake data for admin user
   for (let i = 0; i < 10; i += 1) {
@@ -18,31 +18,37 @@ db.once('open', async() => {
     const email = faker.internet.email(username);
     const password = faker.internet.password();
     const age = faker.random.number({max: 100});
-    const admin = true
     const children = []
 
     // append to userData
-    userData.push({ username, email, password, age, admin, children })
+    userData.push({ username, email, password, age, children })
   } 
 
-  const createdUsers = await User.collection.insertMany(userData);
+  // insert users to model
+  await User.collection.insertMany(userData);
 
-  // // create children
-  // const childData = []
-  // for (let i = 0; i < 10; i += 1) {
-  //   const randomUserIndex = Math.floor(Math.random() * userData.length);
-  //   const { _id: userId } = userData[randomUserIndex];
+  // create child data
+  for (let i = 0; i < 10; i += 1) {
+    const username = faker.internet.userName();
+    const email = faker.internet.email(username);
+    const password = faker.internet.password();
+    const age = faker.random.number({max: 100});
 
-  //   // establish child data
-  //   const username = faker.internet.userName();
-  //   const email = faker.internet.email(username);
-  //   const password = faker.internet.password();
-  //   const age = faker.random.number({max: 100});
-  //   const admin = false
-  //   const children = []
+    // find parent at random 
+    const randomIndex = Math.floor(Math.random() * userData.length);
+    console.log(randomIndex)
+    const { _id: userId } = userData[randomIndex];
+    const parent = userId
 
-  //   User.updateOne({_id: userId}, {$push: {children: {username, email, password, age, admin, children }}})
-  // }
+    // append child to parent
+    const createdChild = await Child.create({ username, email, password, age, parent });
+
+    await User.updateOne(
+      { _id: userId },
+      { $push: { children: createdChild }}
+    )
+  }
 
   process.exit(0);
 })
+
