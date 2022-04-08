fa9ea19b8ba6fb83c45a312a1aba03b86f999a39
brittany spratford <britsprat@gmail.com>
2022-04-07 20:32:27 -0700

    react styles for background image & font styles edited

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,8 @@ import { setContext } from '@apollo/client/link/context';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import coverImage from "./assets/images/chores-image.jpg";
+import { makeStyles } from '@material-ui/core/styles'
 
 // import Home from './pages/Home';
 import Login from './pages/Login';
@@ -19,6 +20,7 @@ import NoMatch from './pages/NoMatch';
 import SingleTask from './pages/SingleTask';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
+import AdminProfile from './components/AdminProfile';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -39,23 +41,59 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+const useStyles = makeStyles(theme => ({
+  imageContainer: {
+    display: 'flex',
+    minHeight: '600px',
+    padding: '30px',
+    flexDirection: 'column',
+    flexGrow: 1,
+  },
+  mainContainer: {
+    zIndex: 1,
+  },
+  overlay: {
+    content: "",
+    position: 'absolute',
+    top: '0px',
+    right: '0px',
+    bottom: '0px',
+    left: '0px',
+    background: 'linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, .8))',
+  }
+}))
+
 function App() {
+  const classes = useStyles();
+  const imageBackground = {
+    position: 'relative',
+    backgroundSize: 'cover',
+    backgroundImage: 'url("' + coverImage + '")',
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+  };
+
   return (
     <ApolloProvider client={client}>
       <Router>
 
-        <div className="flex-column justify-flex-start min-100-vh">
+        <div className="flex-column justify-flex-start min-100-vh" >
           <Header />
-          <div className="container">
+          <div className={classes.imageContainer} style={imageBackground}>
+            <div className={classes.overlay}></div>
+
+            <div className={classes.mainContainer}>
             <Switch>
              {/* <Route exact path="/" component={Home} /> */}
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/profile/:username?" component={Profile} />
               <Route exact path="/SingleTask/:id" component={SingleTask} />
+              <Route exact path="/Admin" component={AdminProfile} />
 
               <Route component={NoMatch} />
             </Switch>
+            </div>
           </div>
           <Footer />
         </div>
--- client/src/components/AdminProfile/index.js
+++ client/src/components/AdminProfile/index.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import coverImage from "../../assets/images/chores-image.jpg";
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  imageContainer: {
+    display: 'flex',
+    minHeight: '600px',
+    padding: '30px',
+    flexDirection: 'column',
+    flexGrow: 1,
+  }
+}))
 
 const AdminProfile = () => {
 
+  const classes = useStyles();
+  const imageBackground = {
+    position: 'relative',
+    backgroundSize: 'cover',
+    backgroundImage: 'url("' + coverImage + '")',
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+  };
+
   return (
-    <div>
+    <div className={classes.imageContainer} style={imageBackground}> 
       <section className="w-100 mt-auto bg-secondary p-4">
         <div className="container"></div>
+        {/* <img src={coverImage} className="background" style={{ width: "100%" }} alt="cover" /> */}
       </section>
     </div>
   );
 };
+
+
 
 export default AdminProfile;
