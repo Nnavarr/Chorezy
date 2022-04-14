5f5b9e5922e203a51f6c60bc9f05b58b4d8e175c
brittany spratford <britsprat@gmail.com>
2022-04-13 18:22:32 -0700

    centering

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -50,15 +50,6 @@ const Profile = () => {
 
     return (
       <div className="flex-row justify-space-between mb-3">
-<<<<<<< HEAD
-      
-      <div className="col-12 mb-3 col-lg-8">
-        <TaskList
-          tasks={user.assignments}
-          title={`${user.username}'s tasks...`}
-        />
-      </div></div>
-=======
         <div className="col-12 mb-3 col-lg-8">
           <TaskList
             tasks={user.assignments}
@@ -67,7 +58,6 @@ const Profile = () => {
         </div>
       </div>
 
->>>>>>> main
     )
   }
 
